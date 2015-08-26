39cd8534f8ff8138065e6669f1db9d590f408445
Jonathon <jeog.dev@gmail.com>
2015-08-26 19:52:53 -0400

    add neg support and +/- overloads to TrimmedRational

--- trimmed_rational.hpp
+++ trimmed_rational.hpp
@@ -21,23 +21,26 @@ along with this program.  If not, see http://www.gnu.org/licenses.
 #include <ratio>
 #include <cmath>
 
-template< typename IncrementRatio=std::ratio<1,1>,
-          double(*AdjustFunc)(double)=round,
-          unsigned long RoundingDigits=5 >
+template< typename IncrementRatio = std::ratio<1,1>,
+          double(*RoundFunc)(double) = round,
+          unsigned long RoundDigits = 5 >
 class TrimmedRational{
   static_assert(!std::ratio_greater<IncrementRatio,std::ratio<1,1>>::value,
                 "Increment Ratio > ratio<1,1> " );
-  static constexpr long _round_adj = pow(10,RoundingDigits);
+  static constexpr long _round_adj = pow(10,RoundDigits);
 
   div_t _tmp_dt;
 
 public:
+  typedef TrimmedRational<IncrementRatio,RoundFunc,RoundDigits> my_type;
   typedef IncrementRatio increment_ratio;
+  typedef double(*rounding_function)(double);
 
-  static constexpr double increment_size =
-    (double)increment_ratio::num / increment_ratio::den;
-  static constexpr double increments_per_unit =
-      (double)increment_ratio::den / increment_ratio::num;
+  static constexpr unsigned long rounding_digits = RoundDigits;
+  static constexpr double increment_size = (double)increment_ratio::num
+                                                 / increment_ratio::den;
+  static constexpr double increments_per_unit = (double)increment_ratio::den
+                                                      / increment_ratio::num;
 
   const long long whole;
   const long long incr;
@@ -45,20 +48,33 @@ public:
   TrimmedRational(long long whole, long long incr)
     :
       _tmp_dt(div(incr,increments_per_unit)),
-      whole(whole + _tmp_dt.quot),
-      incr(_tmp_dt.rem)
+      whole(whole + (_tmp_dt.rem >= 0 ? _tmp_dt.quot : -_tmp_dt.quot-1)),
+      incr(_tmp_dt.rem + (_tmp_dt.rem >= 0 ? 0 : increments_per_unit))
     {
     }
   TrimmedRational(double r)
-    :
-      whole( (long long)r ),
-      incr(AdjustFunc((r-whole)*increments_per_unit))
+    : /* careful, non explicit constr */
+      whole( r >= 0 ? (long long)r : (long long)r - 1),
+      incr( RoundFunc((r-whole)*increments_per_unit) )
     {
     }
-  operator double() const
+  inline operator double() const
   {
-    return AdjustFunc((whole + incr * increment_size) * _round_adj) / _round_adj;
+    return RoundFunc((whole + incr * increment_size) * _round_adj) / _round_adj;
   }
+  inline my_type operator+(const my_type& r) const
+  {
+    return my_type(this->whole+r.whole, this->incr + r.incr);
+  }
+  inline my_type operator-(const my_type& r) const
+  {
+    return my_type(this->whole - r.whole, this->incr - r.incr);
+  }
+  inline long long as_incr() const
+  {
+    return (this->whole * increments_per_unit + this->incr);
+  }
+  // TODO other arithmetic overloads
 };
 
 #endif
